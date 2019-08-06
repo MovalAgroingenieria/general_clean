396dce7dcff2231353c3b676e92d7b64a3b93701
jsandoval <jsandoval@moval.es>
2019-08-06 10:42:51 +0200

    - Creation of module Partner Street Type. Create a new selection field to choose the type of road/street accordingly to Spanish cadastral codes. - It has as depedence the module Partner Street Number. - Only the main terms have been traslated, the type of road is not translated.

---
+++ partner_street_type/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Street Type",
+    "summary": "This module add the selection field street_type\
+               according to the Spanish cadastre.",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "partner_street_number",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
