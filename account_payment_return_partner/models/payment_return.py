3846263535adbd7e19216be70c2f0223a17dd4ff
jsandoval <jsandoval@moval.es>
2019-08-07 11:21:03 +0200

    Creation of general moval module account_payment_return_partner. - This module overwrite the fields 'expense_partner_id' and 'default_expense_partner_id' to remove filter (domain) by supplier.

---
+++ account_payment_return_partner/models/payment_return.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class PaymentReturnLine(models.Model):
+    _inherit = "payment.return.line"
+
+    # Overwrite original field to remove domain
+    expense_partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Expense partner",
+        domain=[])
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+
+    # Overwrite original field to remove domain
+    default_expense_partner_id = fields.Many2one(
+        comodel_name="res.partner", string="Default Expense Partner",
+        domain=[],
+        help='Default partner for commission expenses')
