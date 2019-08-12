050cf7cabb11d3d7575009daf3814b6776a31fd5
jsandoval <jsandoval@moval.es>
2019-08-12 14:09:00 +0200

    Module creation. Change fields.Text custom_text and custom_text_after_details for fields.Html

---
+++ account_credit_control_html/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Credit Control HTML",
+    "summary": "Change fields.Text for fields.Html",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account_credit_control_dunning_fees",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
