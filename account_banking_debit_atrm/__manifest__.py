3ff595862ba0dc1060992e1ce82ff2e93785948a
jsandoval <jsandoval@moval.es>
2019-08-13 11:24:34 +0200

    - Creation of module account_banking_debit_atrm:   · This module generate a normalized payment file for 'Tax Agency of the Region of Murcia (ATRM)' and associated resume report.   · After installation, it is mandatory to create a payment mode called 'ATRM' associated with the payment method 'Tax Agency of the Region of Murcia (ATRM)'.   · Dependencies: account_banking_pain_base, partner_second_lastname and partner_street_type
    - This module is in beta

---
+++ account_banking_debit_atrm/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Banking Debit ATRM",
+    "summary": "This module generate a normalized payment file for \
+               'Tax Agency of the Region of Murcia (ATRM)'",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account_banking_pain_base",
+        "partner_second_lastname",
+        "partner_street_type",
+    ],
+    "data": [
+        "data/atrm_ine_codes_data.xml",
+        "data/account_payment_method_data.xml",
+        "reports/atrm_resume.xml",
+        "views/account_payment_line_views.xml",
+        "views/account_payment_order_views.xml",
+        "views/ine_codes_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ account_banking_debit_atrm/models/ine_codes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class IneCodes(models.Model):
+    _name = "res.ine.codes"
+    _description = "Codes for cities in the INE (Spanish Statistics National Institute)"
+
+    ine_code_state = fields.Char(string="State INE code")
+    ine_code_province = fields.Integer(string="Province INE code")
+    ine_code_city = fields.Integer(string="City INE code")
+    city_name = fields.Char(string="City")
+    city_name_simplified = fields.Char(string="City simplified")
