377c8f0615a8f2bb869c88404a0799b6fdaa1384
jsandoval <jsandoval@moval.es>
2019-11-05 19:07:09 +0100

    Creation of module account_payment_term_html

---
+++ account_payment_term_html/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Payment Term HTML",
+    "summary": "Change fields.Text for fields.Html",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_view.xml",
+        "reports/report_invoice.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
