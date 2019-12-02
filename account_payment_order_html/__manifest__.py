0179025a89d54aed289bb493e08ed9176c6403cb
jsandoval <jsandoval@moval.es>
2019-12-02 16:31:10 +0100

    account_payment_order_html: Module creation  - Add two HTML comment fields to account_payment_order model  - Those fields are printed in the payment orders reports

---
+++ account_payment_order_html/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 2019 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Payment Order HTML",
+    "summary": "Add two comment HTML fields",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account_payment_order",
+    ],
+    "data": [
+        "views/account_payment_order_views.xml",
+        "reports/account_payment_order.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
