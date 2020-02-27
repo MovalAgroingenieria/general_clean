4563ba54cbe6ecfe13ee02b999c6f959beb2c5fd
jsandoval <jsandoval@moval.es>
2020-02-27 16:20:09 +0100

    [UPD] account_banking_debit_suma: Delete INE codes - The INE codes have been moved to individual module

--- account_banking_debit_suma/__manifest__.py
+++ account_banking_debit_suma/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Banking Debit SUMA",
-    "summary": "This module generate a normalized payment file for (SUMA)'",
+    "summary": "This module generate a normalized payment file for (SUMA)",
     "version": '10.0.1.1.0',
     "category": "Moval General Addons",
     "website": "http://www.moval.es",
@@ -13,15 +13,13 @@
         "account_banking_pain_base",
         "partner_second_lastname",
         "partner_street_type",
+        "l10n_es_ine_codes",
     ],
     "data": [
-        "security/ir.model.access.csv",
-        "data/suma_ine_codes_data.xml",
         "data/account_payment_method_data.xml",
         "reports/suma_resume.xml",
         "views/account_payment_line_views.xml",
         "views/account_payment_order_views.xml",
-        "views/ine_codes_views.xml",
     ],
     "installable": True,
     "application": False,
--- account_banking_debit_suma/models/__init__.py
+++ account_banking_debit_suma/models/__init__.py
@@ -2,4 +2,3 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import account_payment_order
-from . import ine_codes
