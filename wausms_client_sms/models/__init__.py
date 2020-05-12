822e01d8ae49bef555037fc862ae0627977c7e3e
Juanu Sandoval <jsandoval@moval.es>
2020-05-13 00:18:19 +0200

    [NEW] Creation of branch 13.0-master

--- wausms_client_sms/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# 2019 Moval Agroingeniería
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import wausms_config_settings
-from . import wausms_tracking
-from . import res_partner
-
