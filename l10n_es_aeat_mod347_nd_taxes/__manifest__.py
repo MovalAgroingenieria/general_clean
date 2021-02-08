fb5e6a9d039b6ff0203e1c39644fe37593a9d8ec
Juanu Sandoval <jsandoval@moval.es>
2021-02-08 17:13:24 +0100

    [NEW] l10n_es_aeat_mod347_nd_taxes: Module creation

---
+++ l10n_es_aeat_mod347_nd_taxes/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': "AEAT modelo 347 non deductible taxes",
+    'version': "10.0.1.1.2",
+    'author': "Moval Agroingeniería",
+    'website': "http://www.moval.es",
+    'category': "Accounting",
+    'license': "AGPL-3",
+    'depends': [
+        "l10n_es_aeat_mod347",
+    ],
+    'data': [
+        "data/tax_code_map_mod347_data.xml",
+    ],
+    'installable': True,
+}
