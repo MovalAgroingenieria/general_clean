b7508360338c7ef3998e95f67779bbb4c28adcaf
Juanu Sandoval <jsandoval@moval.es>
2021-03-10 10:50:15 +0100

    [NEW] hr_attendance_selfedit: Module creation

---
+++ hr_attendance_selfedit/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Attendances Self Edit",
+    "summary": "Track and edit employee attendance",
+    "version": "13.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "category": "Human Resources/Attendances",
+    "depends": [
+        "hr_attendance",
+    ],
+    "data": [
+        'views/hr_attendance_view.xml',
+    ],
+    "installable": True,
+    "application": False,
+}
