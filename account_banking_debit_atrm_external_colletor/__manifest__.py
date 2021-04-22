18a3f68bfeb17147a4d21cfb7071c03272907ced
Juanu Sandoval <jsandoval@moval.es>
2021-04-22 11:17:57 +0200

    [NEW] account_banking_debit_atrm_external_colletor: Module creation

---
+++ account_banking_debit_atrm_external_colletor/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Banking Debit ATRM: External Collector",
+    "summary": "Change ATRM to External Collector",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+    	"account_banking_debit_atrm",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
