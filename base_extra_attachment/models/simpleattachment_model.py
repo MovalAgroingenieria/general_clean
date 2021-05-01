6b39dee826301ccbeec85c5a1f2a3455c4b65b7e
Eduardo Iniesta <einiesta@moval.es>
2021-05-01 13:32:25 +0200

    [NEW] base_extra_attachment: Module creation (with abstract model to manage attachments)

---
+++ base_extra_attachment/models/simpleattachment_model.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import models, fields, api
+
+
+class SimpleattachmentModel(models.AbstractModel):
+    _name = 'simpleattachment.model'
+    _description = 'Simple-Attachment Model'
+
+    number_of_attachments = fields.Integer(
+        string='Number of attachments',
+        compute='_compute_number_of_attachments')
+
+    has_attachments = fields.Boolean(
+        string='Has attachments',
+        compute='_compute_has_attachments',
+        search='_search_has_attachments')
+
+    @api.multi
+    def _compute_number_of_attachments(self):
+        ir_attachment_model = self.env['ir.attachment'].sudo()
+        for record in self:
+            number_of_attachments = 0
+            res_model = self.__class__._name
+            res_id = record.id
+            attachments = ir_attachment_model.search(
+                [('res_model', '=', res_model),
+                 ('res_id', '=', res_id)])
+            if attachments:
+                number_of_attachments = len(attachments)
+            record.number_of_attachments = number_of_attachments
+
+    @api.multi
+    def _compute_has_attachments(self):
+        for record in self:
+            has_attachments = False
+            if record.number_of_attachments > 0:
+                has_attachments = True
+            record.has_attachments = has_attachments
+
+    def _search_has_attachments(self, operator, value):
+        with_attachments = ((operator == '=' and value) or
+                            (operator == '!=' and not value))
+        ids = self.get_ids_of_model_with_attachments(
+            self.__class__._name, with_attachments)
+        return ([('id', '=', ids)])
+
+    def get_ids_of_model_with_attachments(self,
+                                          model_name, with_attachments=True):
+        resp = []
+        sql_with_attachments = 'select distinct res_id from ir_attachment ' + \
+            'where res_model = \'account.invoice\''
+        sql_without_attachments = 'select id from account_invoice ' + \
+            'where id not in (' + sql_with_attachments + ')'
+        sql = sql_with_attachments
+        if (not with_attachments):
+            sql = sql_without_attachments
+        self.env.cr.execute(sql)
+        sql_resp = self.env.cr.fetchall()
+        if sql_resp:
+            for item in sql_resp:
+                resp.append(item[0])
+        return resp
