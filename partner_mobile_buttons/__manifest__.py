8556572adca0d94eb6e95345f0cc29b8c4e5dfc3
Juanu Sandoval <jsandoval@moval.es>
2021-05-23 17:25:46 +0200

    [NEW] partner_mobile_buttons: Module creation

---
+++ partner_mobile_buttons/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner mobile buttons",
+    "summary": "Add mobile buttons to partner views",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        'base',
+    ],
+    "data": [
+        "views/resources.xml",
+        'views/res_partner_view.xml',
+    ],
+    "installable": True,
+    "application": False,
+}
