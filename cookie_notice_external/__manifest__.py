16edb8db39c467c06f4284418a1737dcd095d8d1
Salvador Sánchez Aroca <ssanchez@moval.es>
2021-09-01 16:46:17 +0200

    [WIP]

---
+++ cookie_notice_external/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Cookie Notice External',
+    'summary': """Insert a cookie URL service""",
+    "version": '13.0.1.1.0',
+    "category": "Tools",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        'website',
+    ],
+    "data": [
+        "views/res_config_settings_views.xml",
+        "templates/website.xml",
+    ],
+    'images': ['static/description/banner.png'],
+    "installable": True,
+    "application": False,
+    "price": 5.0,
+    "currency": "EUR",
+}
