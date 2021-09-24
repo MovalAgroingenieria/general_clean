faf344a548430cc8f379d97afebc80a09bc4d43f
Salvador Sánchez Aroca <ssanchez@moval.es>
2021-09-24 15:41:12 +0200

    [NEW] portal_my_details_extra_fields: Module created for adding more fields to portal my details page.

---
+++ portal_my_details_extra_fields/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Portal Web Cliente My Details Add Extra Fields",
+    "summary": "Add some extra fields on Portal User My Details Web View",
+    "version": "13.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "category": "Web",
+    "depends": [
+        "portal",
+    ],
+    "data": [
+        "views/portal_templates.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
