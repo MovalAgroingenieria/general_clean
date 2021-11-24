699bb3ecc2d909f0fc1a1d02b2d7dd70d6c355bb
Juanu Sandoval <jsandoval@moval.es>
2021-11-24 10:40:10 +0100

    [NEW] fontawesome_ext: v14 module creation

---
+++ fontawesome_ext/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Fontawesome Extended',
+    'summary': """Add custom fontawesome fonts.""",
+    "version": '14.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        'web',
+    ],
+    "data": [
+        'templates/assets.xml',
+    ],
+    "installable": True,
+    "application": False,
+}
+
