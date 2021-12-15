6871cf735f6b19a4efde8fd804644987863f19e3
Juanu Sandoval <jsandoval@moval.es>
2021-12-15 15:25:07 +0100

    [NEW] account_move_analytic_tags: Module creation

---
+++ account_move_analytic_tags/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Move Analytic Tags",
+    "summary": "Add analytical tags to the manual journal entries",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "price": 0.0,
+    "currency": "EUR",
+    "images": ["static/description/banner.png"],
+}
