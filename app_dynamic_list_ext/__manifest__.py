a61fdf275e2f46123083123f6a04e8b9cc6365f1
Salvador Sanchez Aroca <ssanchez@moval.es>
2021-12-30 11:25:12 +0100

    [NEW] app_dynamic_list_ext: Module creation.
     * Module that ensures that the "set columns" button is always visible.

---
+++ app_dynamic_list_ext/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "App Dynamic List Extended",
+    "summary": "Functionality to modify Set columns button for avoiding"
+               "",
+    "version": "10.0.1.0.0",
+    "category": "Tools",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "data": [
+    ],
+    "depends": [
+        "app_dynamic_list",
+    ],
+    "application": False,
+    "installable": True,
+    'qweb': ['static/src/xml/listview_button_view.xml'],
+}
