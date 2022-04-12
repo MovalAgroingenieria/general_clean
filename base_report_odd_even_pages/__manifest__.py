5e605450f7f3e41f2aa1a46502a1bcc2b9910b79
Salvador Sanchez Aroca <ssanchez@moval.es>
2022-04-12 17:05:31 +0200

    [NEW] base_report_odd_even_pages: Module creation on v14.

---
+++ base_report_odd_even_pages/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# 2022 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Base Report Odd Even Pages",
+    "summary": "Functionality to modify report generated ensuring and odd or"
+               "even number of pages.",
+    "version": "14.0.1.1.0",
+    "category": "Tools",
+    "website": "https://moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "data": [
+        "views/ir_actions_views.xml",
+    ],
+    "depends": [
+        "base",
+    ],
+    'images': ['images/moval.png'],
+    "application": False,
+    "installable": True,
+    "price": 5.0,
+    "currency": "EUR",
+    "images": ["static/description/banner.png"],
+}
