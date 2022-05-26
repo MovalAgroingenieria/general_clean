f8002995330f641f4e42fc13ea0fd08ea4c8a1f3
Juanu Sandoval <jsandoval@moval.es>
2022-05-26 13:47:02 +0200

    [UPD] Migration v14 several modules: * hr_attendance_selfedit * hr_attendance_with_lastnames * change icons in base_extra_gis, base_gen, base_loc

---
+++ hr_attendance_with_lastnames/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2022 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Attendances Fix for lastnames",
+    "summary": "Add fields of lastnames to Employee to avoid errors",
+    "version": "14.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "category": "Human Resources/Attendances",
+    "depends": [
+        "hr_attendance",
+        "hr_employee_lastnames",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
