904b336298cee548045a29064843810d408553ba
Salvador Sanchez Aroca <ssanchez@moval.es>
2022-07-20 16:28:09 +0200

    [UPD] account_banking_sepa_direct_debit_extended: Module created to extend Cron that calculates if Mandates expired, so last_debit_date is updated before the check.

---
+++ account_banking_sepa_direct_debit_extended/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# 2022 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Banking SEPA Direct Debit Extended",
+    "summary": "This module added an update method before the cron that sets "
+               "the mandates state to expired",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account_banking_sepa_direct_debit",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ account_banking_sepa_direct_debit_extended/models/account_banking_mandate.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# 2022 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, api
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import logging
+
+NUMBER_OF_UNUSED_MONTHS_BEFORE_EXPIRY = 36
+
+logger = logging.getLogger(__name__)
+
+
+class AccountBankingMandate(models.Model):
+    _inherit = 'account.banking.mandate'
+
+    @api.model
+    def _sdd_mandate_set_state_to_expired(self):
+        expire_limit_date = datetime.today() + \
+            relativedelta(months=-NUMBER_OF_UNUSED_MONTHS_BEFORE_EXPIRY)
+        expire_limit_date_str = expire_limit_date.strftime('%Y-%m-%d')
+        expired_mandates = self.search(
+            ['|',
+             ('last_debit_date', '=', False),
+             ('last_debit_date', '<=', expire_limit_date_str),
+             ('state', '=', 'valid'),
+             ('signature_date', '<=', expire_limit_date_str)])
+        for posible_expire_mandate in expired_mandates:
+            if (posible_expire_mandate.payment_line_ids and
+                    len(posible_expire_mandate.payment_line_ids) > 0):
+                if (posible_expire_mandate.last_debit_date !=
+                        posible_expire_mandate.payment_line_ids[-1]):
+                    posible_expire_mandate.last_debit_date = \
+                        posible_expire_mandate.payment_line_ids[-1].date
+        return super(
+            AccountBankingMandate, self)._sdd_mandate_set_state_to_expired()
