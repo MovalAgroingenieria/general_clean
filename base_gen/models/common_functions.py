889e9e3de4763c1043d5cf1b580ff16f8005642c
Salvador Sanchez Aroca <ssanchez@moval.es>
2022-09-21 15:00:16 +0200

    [UPD] base_gen: Added transform_date_to_locale function.

--- base_gen/models/common_functions.py
+++ base_gen/models/common_functions.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
-# 2021 Moval Agroingeniería
+# 2022 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-import locale
-from odoo import models, api
+from odoo import models, fields
 
 
 class CommonFunctions(models.AbstractModel):
     _name = 'common.functions'
     _description = 'Common functions to be used by other models'
 
-    def transform_float_to_locale(self, float_number, precision):
+    def transform_float_to_locale(self, float_number, precision, lang=False):
         precision = '%.' + str(precision) + 'f'
-        lang = 'es_ES'
-        if ('lang' in self.env.context and self.env.context['lang']):
-            lang = self.env.context['lang']
+        if (not lang):
+            lang = 'es_ES'
+            if ('lang' in self.env.context and self.env.context['lang']):
+                lang = self.env.context['lang']
         lang_model = self.env['res.lang'].search([('code', '=', lang)])
         formated_float_number = str(float_number)
         if (lang_model):
             formated_float_number = \
                 lang_model.format(precision, float_number, True)
         return formated_float_number
+
+    def transform_date_to_locale(self, date, lang=False):
+        if (not lang):
+            lang = 'es_ES'
+            if ('lang' in self.env.context and self.env.context['lang']):
+                lang = self.env.context['lang']
+        lang_model = self.env['res.lang'].search([('code', '=', lang)])
+        formated_date_str = str(date)
+        if (lang_model):
+            formated_date_str = date.strftime(lang_model.date_format)
+        return formated_date_str
 
     def get_value_from_translation(self, module, src, lang=None):
         resp = src
