01ab9691e00f672979b77ec79eed2061efe74522
Juanu Sandoval <jsandoval@moval.es>
2023-03-28 14:27:27 +0200

    [NEW] base_bank_extended: Module creation * Module to modify base res_bank model * Modification: Allow the same bank account number in several partners

---
+++ base_bank_extended/__manifest__.py
@@ -0,0 +1,18 @@
+# 2023 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Resource Bank Extended",
+    "summary": "Extended functionality of res_bank base module",
+    "version": "14.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "category": "Hidden",
+    "depends": [
+        "base",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ base_bank_extended/models/__init__.py
@@ -0,0 +1,4 @@
+# 2023 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from . import res_bank
