7e0d2da49711dec67e43e6cd6034dec979cc0564
Juanu Sandoval <jsandoval@moval.es>
2023-09-26 11:06:10 +0200

    [NEW] account_payment_partner_extended: Module creation

---
+++ account_payment_partner_extended/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# 2023 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Payment Partner Extended',
+    'summary': """Extend the functionality of parent module""",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account_payment_partner",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
