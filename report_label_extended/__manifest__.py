6a7c202b3be4fac4f1d1143009ab526077b92f68
JMARTINEZ <jmartinez@moval.es>
2023-10-18 17:41:22 +0200

    [NEW] report_label_extended: module creation

---
+++ report_label_extended/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2020 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Report Labels Extended",
+    "summary": "This module extends parent module functionality.",
+    "version": '14.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "report_label",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
