8475a317041afcfe7883dc995fa8a962c02384cb
Juanu Sandoval <jsandoval@moval.es>
2023-11-10 10:15:52 +0100

    [UPD] hr_attendance_staff_manager: Delete unused imports

--- hr_attendance_staff_manager/models/hr_attendance.py
+++ hr_attendance_staff_manager/models/hr_attendance.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from lxml import etree
-from odoo import models, fields, api, exceptions, _
+from odoo import models, api
 
 
 class HrAttendance(models.Model):
