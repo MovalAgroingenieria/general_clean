9d81259b2eb166bba63f42744605700bc456424f
Juanu Sandoval <jsandoval@moval.es>
2024-01-16 14:24:06 +0100

    [UPD] bundle_user_experience_modules: Add module odoo_user_login_security

--- bundle_user_experience_modules/__manifest__.py
+++ bundle_user_experience_modules/__manifest__.py
@@ -3,21 +3,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    "name": "Bundle user experience modules",
-    "summary": "Installation of common modules related to the user experience",
-    "version": "14.0.1.0.0",
-    "author": "Moval Agroingeniería",
-    "license": "AGPL-3",
-    "category": "Hidden",
-    "depends": [
-        "base",
-        "disable_odoo_online",
-        "web_listview_range_select",
-        "web_sheet_full_width",
-        "web_tree_dynamic_colored_field",
-        "entrivis_kanban_many2many_tags",
+    'name': 'Bundle user experience modules',
+    'summary': 'Installation of common modules related to the user experience',
+    'version': '14.0.1.0.0',
+    'author': 'Moval Agroingeniería',
+    'license': 'AGPL-3',
+    'category': 'Hidden',
+    'depends': [
+        'base',
+        'disable_odoo_online',
+        'web_listview_range_select',
+        'web_sheet_full_width',
+        'web_tree_dynamic_colored_field',
+        'entrivis_kanban_many2many_tags',
+        'odoo_user_login_security',
     ],
-    "data": [],
-    "installable": True,
-    "application": False,
+    'data': [],
+    'installable': True,
+    'application': False,
 }
