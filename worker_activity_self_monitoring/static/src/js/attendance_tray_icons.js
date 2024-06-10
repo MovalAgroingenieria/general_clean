7e5df7b990ae742949830b1adfbb11f9fcb51af7
JMARTINEZ <jmartinez@moval.es>
2024-06-10 17:23:45 +0200

    [UPD] worker_activity_self_monitoring: changes on icons display

--- worker_activity_self_monitoring/static/src/js/attendance_tray_icons.js
+++ worker_activity_self_monitoring/static/src/js/attendance_tray_icons.js
@@ -6,8 +6,6 @@ odoo.define('worker_activity_self_monitoring.TrayIcon', function (require) {
     var rpc = require('web.rpc');
     var session = require('web.session');
 
-    console.log('Starting TrayIcon module');
-
     var TrayIcon = Widget.extend({
         template: 'tray_icon',
         events: {
@@ -15,123 +13,65 @@ odoo.define('worker_activity_self_monitoring.TrayIcon', function (require) {
         },
         start: function () {
             this._super.apply(this, arguments);
-            console.log('TrayIcon Widget started');
             this.updateIcon();
-
-            // Suscribirse a eventos específicos de hr_attendance
             core.bus.on('attendance_gauge_check_in', this, this.onCheckIn);
             core.bus.on('attendance_gauge_check_out', this, this.onCheckOut);
-
-            // Insertar el icono en el lugar adecuado del DOM
             this.insertIcon();
-
-            // Obtener la URL dinámica para la vista de asistencias
-            this.setDynamicAttendanceURL();
         },
         insertIcon: function () {
-            // Evitar duplicados removiendo cualquier instancia existente
             $(".o_menu_systray .o_tray_icon_li").remove();
-
-            // Busca el contenedor de la bandeja del sistema
             var $systray = $(".o_menu_systray");
             if ($systray.length) {
-                // Crea un nuevo elemento <li> y añade el contenido del widget
                 var $li = $("<li>", { class: "o_tray_icon_li" });
                 $li.append(this.$el);
-                $systray.prepend($li); // Inserta el nuevo <li> al principio de la lista
+                $systray.prepend($li);
             }
         },
-        setDynamicAttendanceURL: function () {
-            var self = this;
-
-            rpc.query({
-                model: 'ir.ui.menu',
-                method: 'search_read',
-                args: [[['name', '=', 'Asistencias']], ['id']],
-                limit: 1,
-            }).then(function (menus) {
-                if (menus.length > 0) {
-                    var menu_id = menus[0].id;
-                    return rpc.query({
-                        model: 'ir.actions.client',
-                        method: 'search_read',
-                        args: [[['tag', '=', 'hr_attendance_my_attendances']], ['id']],
-                        limit: 1,
-                    }).then(function (actions) {
-                        if (actions.length > 0) {
-                            var action_id = actions[0].id;
-                            var url = `/web#menu_id=${menu_id}&action=${action_id}`;
-                            self.$('.o_AttendanceMenu_toggler').attr('href', url);
-                            console.log('Dynamic URL set for attendance:', url);
-                        } else {
-                            console.error('Action not found for attendance');
-                        }
-                    });
-                } else {
-                    console.error('Menu not found for attendance');
-                }
-            }).catch(function (error) {
-                console.error('Error fetching dynamic URL for attendance:', error);
-            });
-        },
         onCheckIn: function () {
-            console.log('Check-in detected');
             this.updateIcon();
             this.scheduleUpdate();
         },
         onCheckOut: function () {
-            console.log('Check-out detected');
             this.updateIcon();
-            this.clearTimers(); // Detiene la actualización periódica
+            this.clearTimers();
         },
         updateIcon: function () {
             var self = this;
-            console.log('Fetching data to update icon...');
             rpc.query({
                 model: 'hr.employee',
                 method: 'search_read',
                 args: [[['user_id', '=', session.uid]], ['id', 'attendance_state', 'hours_today']],
                 limit: 1,
             }).then(function (employees) {
-                console.log('Employee data fetched:', employees);
                 if (employees.length > 0) {
                     var employee = employees[0];
                     self.attendanceState = employee.attendance_state;
-                    self.hoursToday = employee.hours_today || 0; // Horas trabajadas hasta ahora en formato decimal
-                    self.lastUpdateTime = new Date(); // Hora exacta de la última actualización
+                    self.hoursToday = employee.hours_today || 0;
+                    self.lastUpdateTime = new Date();
 
                     self.updateAttendanceStatusIcon();
 
                     if (self.attendanceState === 'checked_in') {
                         self.scheduleUpdate();
                     } else {
-                        self.clearTimers(); // Detener la actualización si no está `checked_in`
+                        self.clearTimers();
                     }
                 }
             }).catch(function (error) {
-                console.error('Error fetching employee data:', error);
                 self.clearTimers();
             });
         },
         scheduleUpdate: function () {
             var self = this;
 
-            // Detener cualquier temporizador existente
             this.clearTimers();
 
-            // Calcular los segundos residuales para sincronizar con el minuto completo
             var totalSecondsWorked = Math.floor(self.hoursToday * 3600);
             var residual_seconds = totalSecondsWorked % 60;
             var millisecondsUntilNextMinute = (60 - residual_seconds) * 1000;
 
-            console.log(`Residual seconds: ${residual_seconds}, waiting ${millisecondsUntilNextMinute} milliseconds for synchronization.`);
-
-            // Configurar `setTimeout` para esperar hasta el próximo minuto completo solo si está `checked_in`
             this.syncTimeout = setTimeout(function () {
-                console.log('Synchronizing with the next complete minute.');
-                // Luego de sincronizarse con el siguiente minuto completo, actualiza cada minuto
                 self.updateInterval = setInterval(self.updateDisplayedTime.bind(self), 60000);
-                // Actualiza inmediatamente después de sincronizarse con el siguiente minuto completo
                 self.updateDisplayedTime();
             }, millisecondsUntilNextMinute);
         },
@@ -150,8 +90,6 @@ odoo.define('worker_activity_self_monitoring.TrayIcon', function (require) {
             } else {
                 $icon.addClass('red_circle');
             }
-
-            // Actualiza la visualización de horas basada en el último tiempo exacto
             this.updateDisplayedTime();
         },
         updateDisplayedTime: function () {
@@ -162,7 +100,7 @@ odoo.define('worker_activity_self_monitoring.TrayIcon', function (require) {
                 var additionalMinutes = secondsSinceLastUpdate / 60;
                 currentHours += additionalMinutes / 60;
             }
-            var totalSecondsWorked = Math.floor(currentHours * 3600); // Total de segundos trabajados hasta ahora
+            var totalSecondsWorked = Math.floor(currentHours * 3600);
             var hours = Math.floor(totalSecondsWorked / 3600);
             var minutes = Math.floor((totalSecondsWorked % 3600) / 60);
 
@@ -174,18 +112,15 @@ odoo.define('worker_activity_self_monitoring.TrayIcon', function (require) {
             console.log(`Updated displayed time: ${formattedHours}`);
         },
         navigateToAttendance: function (event) {
-            // Prevenir la navegación predeterminada
             event.preventDefault();
-            // No hacer nada aquí ya que el enlace <a> se encarga de la navegación
         },
         destroy: function () {
-            this.clearTimers(); // Detener el intervalo al destruir el widget
+            this.clearTimers();
             this._super.apply(this, arguments);
         }
     });
 
     core.bus.on('web_client_ready', null, function () {
-        // Añadir el icono al cargar la página
         var trayIcon = new TrayIcon();
         trayIcon.appendTo($('body'));
     });
--- worker_activity_self_monitoring/static/src/js/task_tray_icons.js
+++ worker_activity_self_monitoring/static/src/js/task_tray_icons.js
@@ -6,8 +6,6 @@ odoo.define('worker_activity_self_monitoring.TaskIcon', function (require) {
     var rpc = require('web.rpc');
     var session = require('web.session');
 
-    console.log('Starting TaskIcon module');
-
     var TaskIcon = Widget.extend({
         template: 'task_icon',
         events: {
@@ -15,37 +13,30 @@ odoo.define('worker_activity_self_monitoring.TaskIcon', function (require) {
         },
         start: function () {
             this._super.apply(this, arguments);
-            console.log('TaskIcon Widget started');
-            this.updateIcon();
-
-            // Insertar el icono en el lugar adecuado del DOM
             this.insertIcon();
+            this.updateIcon();
         },
         insertIcon: function () {
-            // Evitar duplicados removiendo cualquier instancia existente
             $(".o_menu_systray .o_task_icon_li").remove();
 
-            // Busca el contenedor de la bandeja del sistema
             var $systray = $(".o_menu_systray");
             if ($systray.length) {
-                // Crea un nuevo elemento <li> y añade el contenido del widget
                 var $li = $("<li>", { class: "o_task_icon_li" });
                 $li.append(this.$el);
-                $systray.prepend($li); // Inserta el nuevo <li> al principio de la lista
+                $systray.prepend($li);
             }
         },
         updateIcon: function () {
             var self = this;
-            console.log('Fetching data to update icon...');
             rpc.query({
                 model: 'project.task',
                 method: 'search_read',
                 args: [
                     [
-                        ['starter_user_id', '=', session.uid],  // Filtra por usuario actual
-                        ['task_running', '!=', false],    // Tareas con hora de inicio
+                        ['starter_user_id', '=', session.uid],
+                        ['task_running', '!=', false],
                     ],
-                    ['id', 'name', 'start_time']        // Campos necesarios
+                    ['id', 'name', 'start_time']
                 ],
                 limit: 1,
                 order: 'start_time desc'
@@ -56,29 +47,22 @@ odoo.define('worker_activity_self_monitoring.TaskIcon', function (require) {
                     self.taskId = task.id;
                     self.taskName = task.name;
                     self.taskStartTime = self.convertToUserTimezone(task.start_time);
-
                     self.updateTaskStatusIcon();
-
-                    // Actualiza el href a la tarea activa
-                    self.$('.o_TaskMenu_toggler').attr('href', `/web#id=${self.taskId}&model=project.task&view_type=form`);
+                    $('.o_TaskMenu_toggler').attr('href', `/web#id=${self.taskId}&model=project.task&view_type=form&action=project.action_view_all_task`);
                 } else {
                     self.setNoTask();
                 }
             }).catch(function (error) {
-                console.error('Error fetching task data:', error);
                 self.setNoTask();
             });
         },
         convertToUserTimezone: function (serverDate) {
-            // Convertir fecha del servidor a la zona horaria del usuario
             return moment.utc(serverDate).local().toDate();
         },
         updateTaskStatusIcon: function () {
             var $icon = this.$('#task_status_icon');
             var $taskDisplay = this.$('#task_name_display');
-
             $icon.removeClass('gray_circle green_circle');
-
             if (this.taskId) {
                 $icon.addClass('green_circle');
                 var taskName = this.taskName.length > 30 ? this.taskName.substring(0, 30).trim() + '...' : this.taskName;
@@ -91,24 +75,19 @@ odoo.define('worker_activity_self_monitoring.TaskIcon', function (require) {
         setNoTask: function () {
             var $icon = this.$('#task_status_icon');
             var $taskDisplay = this.$('#task_name_display');
-
             this.taskId = null;
             this.taskName = '';
             this.taskStartTime = null;
             $icon.removeClass('gray_circle green_circle').addClass('gray_circle');
             $taskDisplay.hide();
-
-            // Actualiza el href a la vista de todas las tareas con el contexto
             this.$('.o_TaskMenu_toggler').attr('href', `/web#action=project.action_view_all_task&cids=&menu_id=PROJECT_MENU_ID&search_default_my_tasks=1&all_task=0&search_default_project_tasks=1`);
         },
         navigateToTask: function (event) {
-            event.preventDefault(); // Prevenir la navegación predeterminada
-            // No hacemos nada aquí, ya que el href se ha configurado en `updateIcon`
+            event.preventDefault();
         }
     });
 
     core.bus.on('web_client_ready', null, function () {
-        // Añadir el icono al cargar la página
         var taskIcon = new TaskIcon();
         taskIcon.appendTo($('body'));
     });
