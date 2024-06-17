2251bc8d4f7b5566f829891e0613c920e0e03c0e
Samuel Fernandez <sfernandez@moval.es>
2024-06-17 16:40:04 +0200

    [UPD] wausms_client_sms : Hide technical fields on tracking for portal users

--- wausms_client_sms/models/wausms_tracking.py
+++ wausms_client_sms/models/wausms_tracking.py
@@ -2,7 +2,8 @@
 # 2019 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, fields, api
+from lxml import etree
 
 
 class WauSMSTracking(models.Model):
@@ -88,3 +89,31 @@ class WauSMSTracking(models.Model):
     response_message = fields.Text(
         string="Response message",
         readonly=True)
+
+    @api.model
+    def fields_view_get(self, view_id=None,
+                        view_type='form', toolbar=False, submenu=False):
+        res = super(
+            WauSMSTracking, self).fields_view_get(
+                view_id=view_id,
+                view_type=view_type,
+                toolbar=toolbar,
+                submenu=submenu)
+
+        # Check if portal user
+        is_portal_user = self.env.user.has_group('base.group_portal')
+        if not is_portal_user:
+            return res
+
+        if view_type == 'form':
+            doc = etree.XML(res['arch'])
+
+            for field in ['wausms_user', 'wausms_url', 'user_id']:
+                for node in doc.xpath("//field[@name='%s']" % field):
+                    node.getparent().remove(node)
+            for page in doc.xpath("//page[@name='response_message_tab']"):
+                page.getparent().remove(page)
+
+            res['arch'] = etree.tostring(doc, encoding='unicode')
+
+        return res
