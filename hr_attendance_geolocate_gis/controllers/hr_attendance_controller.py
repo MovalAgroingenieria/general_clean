4d7c70127a97bde08b7460cccbe71fc617ea6d23
Salvador Sanchez Aroca <ssanchez@moval.es>
2024-07-29 10:35:27 +0200

    [UPD] hr_attendance_geolocate_gis: Fixed attendances_from_dates controller to avoid errors on datetime format.

--- hr_attendance_geolocate_gis/controllers/hr_attendance_controller.py
+++ hr_attendance_geolocate_gis/controllers/hr_attendance_controller.py
@@ -6,20 +6,25 @@ from datetime import datetime
 
 class HrAttendanceController(http.Controller):
 
-    @http.route("/attendances_from_dates", auth='user', type='json', methods=['POST'])
-    def get_attendances_from_dates(self, **values):
-        start_date = values.get('start_date')
-        end_date = values.get('end_date')
+    @http.route("/attendances_from_dates", auth='user', type='json',
+                methods=['POST'], csrf=False)
+    def get_attendances_from_dates(self, *args, **kwargs):
+        jsonrequest = request.jsonrequest
+        start_date = jsonrequest.get('start_date')
+        end_date = jsonrequest.get('end_date')
         if not start_date or not end_date:
-            return json.dumps({'error': 'start_date and end_date are required'})
+            return json.dumps(
+                {'error': 'start_date and end_date are required'})
         try:
             start_date_dt = datetime.strptime(start_date, '%Y-%m-%d')
             end_date_dt = datetime.strptime(end_date, '%Y-%m-%d')
         except ValueError:
             return json.dumps({'error': 'Invalid date format. Use YYYY-MM-DD'})
+        start_date_str = start_date_dt.strftime('%Y-%m-%d 00:00:00')
+        end_date_str = end_date_dt.strftime('%Y-%m-%d 23:59:59')
         domain = [
-            ('check_in', '>=', start_date_dt),
-            ('check_out', '<=', end_date_dt)
+            ('check_in', '>=', start_date_str),
+            ('check_out', '<=', end_date_str)
         ]
         attendances = request.env['hr.attendance'].search(domain)
         output = []
