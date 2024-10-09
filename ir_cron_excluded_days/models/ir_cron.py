a07dcf6122f72517ad93dd45e485c004963d2744
Salvador Sanchez Aroca <ssanchez@moval.es>
2024-10-09 11:06:38 +0200

    [UPD] ir_cron_excluded_days: Added check of excluded day on _process_job function.

--- ir_cron_excluded_days/models/ir_cron.py
+++ ir_cron_excluded_days/models/ir_cron.py
@@ -5,6 +5,7 @@
 from odoo import models, api, fields
 from datetime import datetime
 import pytz
+from dateutil.relativedelta import relativedelta
 
 
 class CustomIrCron(models.Model):
@@ -28,3 +29,64 @@ class CustomIrCron(models.Model):
                     super(CustomIrCron, self).method_direct_trigger()
             else:
                 super(CustomIrCron, self).method_direct_trigger()
+
+    @classmethod
+    def _process_job(cls, job_cr, job, cron_cr):
+        """ Run a given job taking care of the repetition.
+
+        :param job_cr: cursor to use to execute the job, safe to
+         commit/rollback
+        :param job: job to be run (as a dictionary).
+        :param cron_cr: cursor holding lock on the cron job row, to use to
+            update the next exec date,
+            must not be committed/rolled back!
+        """
+        _intervalTypes = {
+            'work_days': lambda interval: relativedelta(days=interval),
+            'days': lambda interval: relativedelta(days=interval),
+            'hours': lambda interval: relativedelta(hours=interval),
+            'weeks': lambda interval: relativedelta(days=7*interval),
+            'months': lambda interval: relativedelta(months=interval),
+            'minutes': lambda interval: relativedelta(minutes=interval),
+        }
+        try:
+            with api.Environment.manage():
+                cron = api.Environment(job_cr, job['user_id'], {})[cls._name]
+                # Use the user's timezone to compare and compute datetimes,
+                # otherwise unexpected results may appear. For instance, adding
+                # 1 month in UTC to July 1st at midnight in GMT+2 gives July 30
+                # instead of August 1st!
+                now = fields.Datetime.context_timestamp(cron, datetime.now())
+                nextcall = fields.Datetime.context_timestamp(
+                    cron, fields.Datetime.from_string(job['nextcall']))
+                numbercall = job['numbercall']
+                ok = False
+                excluded_day = False
+                if (job['excluded_days']):
+                    days_to_exclude = [
+                        int(x) for x in job['excluded_days'].split(',')]
+                    today = now.day
+                    excluded_day = today in days_to_exclude
+                while nextcall < now and numbercall:
+                    if numbercall > 0:
+                        numbercall -= 1
+                    if not excluded_day and (not ok or job['doall']):
+                        cron._callback(
+                            job['model'], job['function'], job['args'],
+                            job['id'])
+                    if numbercall:
+                        nextcall += _intervalTypes[
+                            job['interval_type']](job['interval_number'])
+                    ok = True
+                addsql = ''
+                if not numbercall:
+                    addsql = ', active=False'
+                cron_cr.execute(
+                    "UPDATE ir_cron SET nextcall=%s, numbercall=%s" +
+                    addsql+" WHERE id=%s",
+                    (fields.Datetime.to_string(
+                        nextcall.astimezone(pytz.UTC)), numbercall, job['id']))
+                cron.invalidate_cache()
+        finally:
+            job_cr.commit()
+            cron_cr.commit()
