d0c2d82c288d28d8b0ff719af897164994aaae4b
Miguel Ángel Rodríguez <marodriguez@moval.es>
2024-11-05 08:33:01 +0100

    [NEW] fleet_vehicle_odoometer: Create new module for add new field (initial_value, project_id, quantity) in odometer object

---
+++ fleet_vehicle_odometer/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# 2024 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Task Timer Self Edit",
+    "summary": "Track and edit task time",
+    "version": "14.0.1.0.0",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "category": "Human Resources/Attendances",
+    "depends": [
+        'project',
+        'hr_timesheet',
+        'analytic',
+        'hr_timesheet_sheet',
+    ],
+    "data": [
+        'views/project_task_time.xml',
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ fleet_vehicle_odometer/models/fleet_vehicle.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# 2024 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, _
+from odoo.osv import expression
+
+
+class FleetVehicleOdometer(models.Model):
+    _name = 'fleet.vehicle.odometer'
+    _description = 'Odometer log for a vehicle'
+    _order = 'date desc'
+ 
+    initial_value = fields.Float('Initial Odometer Value', group_operator="max")
+    project_id = fields.Many2one('project.project', 'Project', required=True)
+    quantity = fields.Float('Quantity', compute='_compute_qty_odometer_kms')
+
+    @api.depends('value', 'initial_value')
+    def _compute_qty_odometer_kms(self):
+        for record in self:
+            record.quantity = record.value - record.initial_value
