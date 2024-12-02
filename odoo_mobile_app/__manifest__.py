a493c9ca04e094553caeabef40c877c2ec332041
Juanu Sandoval <jsandoval@moval.es>
2024-12-02 11:30:11 +0100

    [NEW] odoo_mobile_app: Add module folder (manifest, README, static resources)

---
+++ odoo_mobile_app/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2024 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Odoo Mobile App",
+    "summary": "Mobile App to connect with Odoo",
+    "version": '10.0.1.0.0',
+    "category": "Mobile",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+    ],
+    "data": [
+        "views/resources.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
