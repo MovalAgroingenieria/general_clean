3cd0071a798d08a4725f0a09c97f850e59be1707
Miguel Ángel Rodríguez <marodriguez@moval.es>
2025-01-02 10:43:59 +0100

    [UPD] fleet_vehicle_odoometer: add trasnlations for module and new funcionality to calculate the initial odometer value, a new filds to mark if there are gaps and mod color of the affected lines

--- fleet_vehicle_odometer/__manifest__.py
+++ fleet_vehicle_odometer/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# 2024 Moval Agroingeniería
+# 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    "name": "Fleet Vehicle Odoometer",
+    "name": "Fleet Vehicle Odometer",
     "summary": "Add init value and project in odometer",
     "version": "14.0.1.0.0",
     "author": "Moval Agroingeniería",
