fce02745ebf14a4a6886422f976d9810aabe6793
Samuel Fernandez <sfernandez@moval.es>
2025-06-19 18:35:18 +0200

    [UPD] portal_ext: Add mail_message model with custom create method to handle internal notes

--- portal_ext/models/__init__.py
+++ portal_ext/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import wizard
-import models
+import mail_message
---
+++ portal_ext/models/mail_message.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2025 Moval Agroingeniería
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, api
+
+
+class MailMessage(models.Model):
+    _inherit = 'mail.message'
+
+    @api.model
+    def create(self, vals):
+        # If it's an internal note without a model,
+        # clear recipients to avoid sending
+        subtype = \
+            self.env['mail.message.subtype'].browse(vals.get('subtype_id'))
+        if (subtype and subtype.internal and not vals.get('model') and
+                vals.get('partner_ids')):
+            vals['partner_ids'] = [(6, 0, [])]
+        return super(MailMessage, self).create(vals)
