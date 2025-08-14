4cac8466653a6656748d8ed6ef7dc76f1e3dcab5
Guillermo Amante <gamante@moval.es>
2025-08-14 08:52:44 +0200

    [UPD] eom_eoffice: Error fixes

--- eom_eoffice/models/eom_electronicfile.py
+++ eom_eoffice/models/eom_electronicfile.py
@@ -65,10 +65,10 @@ class EomElectronicfile(models.Model):
     company_id = fields.Many2one(
         string='Company',
         comodel_name='res.company',
-        required=True,
+        required=False,
         store=True,
         index=True,
-        default='_default_company_id',
+        compute='_default_company_id',
     )
 
     type = fields.Selection(
